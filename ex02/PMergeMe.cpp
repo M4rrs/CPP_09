00c55a85b1191989c44fd357c6ce751f669723ec
Mars <almasnorazman2803@outlook.com>
2023-07-25 19:17:29 +0800

    did a little bit

--- ex02/PMergeMe.cpp
+++ ex02/PMergeMe.cpp
@@ -1,5 +1,7 @@
 #include "PMergeMe.hpp"
 
+/* SCROLL ALL THE WAY DOWN FOR THE MAIN CONSTRUCTOR USED IN THIS PROGRAM */
+
 /*========================== OCF ==========================*/
 
 PMergeMe::PMergeMe( void ) {}
@@ -19,7 +21,7 @@ PMergeMe &PMergeMe::operator=( const PMergeMe &assign ) {
 
 template < typename T >
 T store( std::string arg ) {
-	std::stringstream str(arg);
+	std::istringstream str(arg);
 	int temp;
 	T cont;
 
@@ -32,7 +34,22 @@ T store( std::string arg ) {
 	return (cont);
 }
 
+template <typename T>
+void checkDup( T cont ) {
+
+	std::T::iterator it1;
+	std::T::iterator it2;
+
+	for (it1 = cont.begin(); it1 != cont.end(); it1++) {
+		for (it2 = it1; it2 != cont.end(); it2++) {
+			if (*it1 == *it2)
+				throw std::logic_error("Duplicate values found.");
+		}
+	}
+}
+
 PMergeMe::PMergeMe( std::string arg ) : _before(arg) {
+
 	if (arg.find_first_not_of("0123456789 ") != std::string::npos)
 		throw std::logic_error("Occurence of non-digit values.");
 
